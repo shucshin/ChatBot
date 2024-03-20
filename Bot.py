f1bf78dd37dd92734cf152cd5d2d94d9dfc3dc7f
shucshin <robinucscs@gmail.com>
2024-03-20 13:58:33 -0600

    Token

--- Bot.py
+++ Bot.py
@@ -8,7 +8,7 @@ async def send_message(message, user_message, is_private):
     await message.author.send(response) if is_private else await message.channel.send(response)
 
 def run_discord_bot():
-    TOKEN = 'YOUR_TOKEN' ##Lo puedes encontrar en el portal de Discord Developer => Bot => Reset Token
+    TOKEN = 'MTIwNjU5ODg4NzM3NzMzODM4OA.GkTy-y.9XORDR0cwvpYqhmI-xWgQ9hh07tfrxCaoLeBjs' ##Lo puedes encontrar en el portal de Discord Developer => Bot => Reset Token
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
